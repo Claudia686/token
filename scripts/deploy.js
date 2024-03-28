c585a4ffa7180d42d30fc1acf0d52f72c8b21e65
Claudia686 <claudia_mihaela@rocketmail.com>
2024-03-28 13:06:35 +0000

    UI Setup

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,14 +8,14 @@ async function main() {
   // Setup accounts & variables
 
   const [deployer, token1, token2, account1, account2, hacker, user1, user2] = await ethers.getSigners()
-
+   
   // Deploy the contract
   const Tokens = await ethers.getContractFactory('Tokens');
 
-  const deployedToken1 = await Tokens.deploy('DexCoin', 'DX', '1000')
+  const deployedToken1 = await Tokens.deploy('DexCoin', 'DX', '1000');
   console.log(`DX deploy to: ${await deployedToken1.getAddress()}`)
 
-  const deployedToken2 = await Tokens.deploy('My Token', 'MT', '1000')
+  const deployedToken2 = await Tokens.deploy('My Token', 'MT', '1000');
   console.log(`MT deploy to: ${await deployedToken2.getAddress()}`)
 
 }
--- src/App.js
+++ src/App.js
@@ -1,40 +1,64 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import { Contract } from 'ethers';
+import config from './config.json';
+import TOKEN_ABIS from './abis/token.json';
 import './App.css';
 
-// ABIs
-import token from './abis/token.json';
-
-// Config
-import configToken from './config.json';
-
 function App() {
-
-// Define data
-  
- 
-  return (
     
- <div class="container">
-        <h1>Tokens Transfer or Swap</h1>
-        <div class="token-selection">
-            <label for="token">Select Token:</label>
-            <select id="token">
-                <option value="token1">DX</option>
-                <option value="token2">MT</option>
-            </select>
-        </div>
-        <div class="action-selection">
-            <button>Transfer</button>
-            <button>Swap</button>
-        </div>
-        <div class="perform-action">
-            <button>Connect wallet</button>
-        </div>
-    </div>
+    const loadBlockchainData = async () => {
+        const accounts = await window.ethereum.request({
+            method: 'eth_requestAccounts'
+        })
+        console.log(accounts[0])
 
-    
-  );
+        // Connect Ethers to blockchain
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const {
+            chainId
+        } = await provider.getNetwork()
+        console.log(chainId)
+
+        // Token Smart Contract 
+        const token = new ethers.Contract(config[chainId].MT.address, TOKEN_ABIS, provider)
+        console.log(token.address)
+        const symbol = await token.symbol()
+        console.log(symbol)
+    }
+
+    useEffect(() => {
+        loadBlockchainData()
+    })
+
+    return (
+
+        <
+        div className = "container" >
+        <
+        h1 > Tokens Transfer or Swap < /h1> <
+        div className = "token-selection" >
+        <
+        label htmlFor = "token" > Select Token: < /label> <
+        select id = "token" >
+        <
+        option value = "token1" > DX < /option> <
+        option value = "token2" > MT < /option> < /
+        select > <
+        /div> <
+        div className = "action-selection" >
+        <
+        button > Transfer < /button> <
+        button > Swap < /button> < /
+        div > <
+        div className = "perform-action" >
+        <
+        button > Connect wallet < /button> < /
+        div > <
+        /div>
+
+
+    );
 }
 
 export default App;
